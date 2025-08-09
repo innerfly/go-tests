4889d311d7e07d54e2b33ca05011cb2541c79c5a
om <innerfly>
2025-08-09 16:30:29 +0300

    refactoring

--- main.go
+++ main.go
@@ -2,11 +2,13 @@ package main
 
 import "fmt"
 
-const spanish = "Spanish"
-const french = "French"
-const engHello = "Hello, "
-const spanishHello = "Hola, "
-const frenchHello = "Bonjour, "
+const (
+	spanish      = "Spanish"
+	french       = "French"
+	engHello     = "Hello, "
+	spanishHello = "Hola, "
+	frenchHello  = "Bonjour, "
+)
 
 func main() {
 	fmt.Println(hello("", ""))
@@ -17,13 +19,18 @@ func hello(name, lang string) string {
 		name = "World"
 	}
 
-	prefix := engHello
+	return greetingPrefix(lang) + name
+}
+
+func greetingPrefix(lang string) (prefix string) {
 	switch lang {
 	case spanish:
 		prefix = spanishHello
 	case french:
 		prefix = frenchHello
+	default:
+		prefix = engHello
 	}
 
-	return prefix + name
+	return prefix
 }

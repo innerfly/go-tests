87f93fb7037e98ba54be08919d030ff8cf1783ff
om <innerfly>
2025-08-13 16:21:58 +0300

    methods

---
+++ shapes/shapes.go
@@ -0,0 +1,24 @@
+package shapes
+
+import "math"
+
+func Perimeter(rectangle Rectangle) float64 {
+	return 2 * (rectangle.width + rectangle.height)
+}
+
+type Rectangle struct {
+	width  float64
+	height float64
+}
+
+func (r Rectangle) Area() float64 {
+	return r.width * r.height
+}
+
+type Circle struct {
+	Radious float64
+}
+
+func (c Circle) Area() float64 {
+	return math.Pi * c.Radious * c.Radious
+}

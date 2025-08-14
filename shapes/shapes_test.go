8b86bacf1db1005fb4f1d0c3a513ee8da786fe3b
innerfly <v.sukhoff@gmail.com>
2025-08-14 11:15:50 +0300

    ok

--- shapes/shapes_test.go
+++ shapes/shapes_test.go
@@ -19,8 +19,8 @@ func TestArea(t *testing.T) {
 		shape   Shape
 		hasArea float64
 	}{
-		{name: "Rectangle", shape: Rectangle{Width: 12, Height: 6}, hasArea: 72.0},
-		{name: "Circle", shape: Circle{Radius: 10}, hasArea: 314.1592653589793},
+		{name: "Rectangle", shape: Rectangle{width: 12, height: 6}, hasArea: 72.0},
+		{name: "Circle", shape: Circle{Radious: 10}, hasArea: 314.1592653589793},
 		{name: "Triangle", shape: Triangle{Base: 12, Height: 6}, hasArea: 36.0},
 	}
 
